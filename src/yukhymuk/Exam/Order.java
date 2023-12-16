a9d24b5138fdfdce053c123ac0e187d7a0d4bde9
DianYukh <dianayukhymukua@gmail.com>
2023-12-16 19:45:06 +0100

    Yukhymuk add orderInfo, displayOrder

--- src/yukhymuk/Exam/Order.java
+++ src/yukhymuk/Exam/Order.java
@@ -1,4 +1,26 @@
 package yukhymuk.Exam;
 
+import java.io.*;
+
 public class Order {
+    private static HotDog hotDog;
+
+    public Order(HotDog hotDog) {
+        this.hotDog = hotDog;
+    }
+public String orderInfo(){
+    return "Order Information: " + hotDog.getOrderInfo();
+}
+    private static void displayOrder() {
+        try (
+             BufferedWriter writer = new BufferedWriter(new FileWriter("orders.txt"))) {
+            writer.write("Order");
+            writer.write(hotDog.getOrderInfo());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
