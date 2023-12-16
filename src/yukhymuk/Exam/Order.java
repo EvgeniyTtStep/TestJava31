a694479cb38158889f5335e4a5fd058b411f1598
DianYukh <dianayukhymukua@gmail.com>
2023-12-16 19:56:48 +0100

    Yukhymuk add orderInfo, displayOrder

--- src/yukhymuk/Exam/Order.java
+++ src/yukhymuk/Exam/Order.java
@@ -1,19 +1,25 @@
 package yukhymuk.Exam;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Order {
     private static HotDog hotDog;
+    private Map<String, Integer> ingredients = new HashMap<>();
 
-    public Order(HotDog hotDog) {
+    public Order(Map<String, Integer> ingredients) {
         this.hotDog = hotDog;
+        this.ingredients = ingredients;
     }
-public String orderInfo(){
-    return "Order Information: " + hotDog.getOrderInfo();
-}
+
+    public String orderInfo() {
+        return "Order Information: " + hotDog.getOrderInfo();
+    }
+
     private static void displayOrder() {
         try (
-             BufferedWriter writer = new BufferedWriter(new FileWriter("orders.txt"))) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("orders.txt"))) {
             writer.write("Order");
             writer.write(hotDog.getOrderInfo());
 
